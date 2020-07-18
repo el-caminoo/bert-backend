996c448e135b7bfe89eb04caa75e6601c4ef52d1
Qalac <officialqalac@gmail.com>
2020-07-18 01:05:17 +0100

    initial commiit

--- api.py
+++ api.py
@@ -1,24 +1,15 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
 
-from helper import QA
-
 app = Flask(__name__)
 CORS(app)
 
-model = QA("model")
-
-@app.route("/predict",methods=['POST'])
+@app.route("/",methods=['GET'])
 def predict():
-    doc = request.json["document"]
-    q = request.json["question"]
-    try:
-        out = model.predict(doc,q)
-        return jsonify({"result":out})
-    except Exception as e:
-        print(e)
-        return jsonify({"result":"Model Failed"})
+    return jsonify({"result":"successful deply"})
 
 if __name__ == "__main__":
-    app.run('0.0.0.0',port=8000)
+    app.run(debug=False,port=8080)
+
+
     
